18ce0ac1a37ddb33b6595f2417a695217e3901bb
outrowed <outrowed@gmail.com>
2025-09-08 20:38:07 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#064789',  
+        secondary: '#427aa1',
+        tertiary: '#ebf2fa',
+        dark: '#0f172a',
+      },
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      mode: "jit",
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
